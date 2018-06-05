1b9dd0a85c13ca9daf94eb45a0c2056587d6ea3d
yang <liuyang89116@gmail.com>
2018-06-05 01:31:57 -0700

    Add Dependency Injection for HelloServiceImpl

--- Spring_day02/src/main/java/com/yang/demo/HelloServiceImpl.java
+++ Spring_day02/src/main/java/com/yang/demo/HelloServiceImpl.java
@@ -1,9 +1,13 @@
 package com.yang.demo;
 
+import lombok.Setter;
+
 public class HelloServiceImpl implements HelloService {
+    @Setter
+    private String info;
 
     @Override
     public void sayHello() {
-        System.out.println("Hello Spring!");
+        System.out.println("Hello Spring!" + info);
     }
 }
